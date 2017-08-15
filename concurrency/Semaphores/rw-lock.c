18e1e43db299728da7d720e77eb0653d2eb377f7
Aos <aosdab@gmail.com>
2017-08-15 14:21:13 -0400

    Ch32

--- concurrency/Semaphores/rw-lock.c
+++ concurrency/Semaphores/rw-lock.c
@@ -1,4 +1,5 @@
 // Read-write lock using semaphores
+// Only allows write when no readers
 
 #include <stdio.h>
 #include <semaphore.h>
@@ -13,7 +14,7 @@ void rwlock_init(rwlock_t *rw) {
   rw->readers = 0;
   sem_init(&rw->lock, 0, 1);
   sem_init(&rw->writelock, 0, 1);
-}
+} 
 
 void rwlock_acquire_readlock(rwlock_t *rw) {
   sem_wait(&rw->lock);
